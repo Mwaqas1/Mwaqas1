4d81408721201da9ded61374a60e434875952a08
Mian waqas <61832872+Mwaqas1@users.noreply.github.com>
2023-08-01 16:41:40 +0500

    Update supercheckout.js

--- views/js/front/supercheckout.js
+++ views/js/front/supercheckout.js
@@ -1,17 +1,4 @@
-/**
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future.If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- * We offer the best and most useful modules PrestaShop and modifications for your online store.
- *
- * @category  PrestaShop Module
- * @author    knowband.com <support@nowband.com>
- * @copyright 2016 Knowband
- * @license   see file: LICENSE.txt
- */
-// changes by rishabh jain
+Free to edit
 
 var check_guest_checkout = "";
 
